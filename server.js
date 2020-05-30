0f786e30319f8f6a6f6b23644779a7c426c5f6e2
sukh <sukhveer.sangha@tatacommunications.com>
2020-05-30 17:23:44 +0100

    routes created using express router

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB()
 
 app.get('/', (req, res) => res.send('API running'))
 
+// Define Routes
+app.use('/api/users', require('./routes/API/users'))
+app.use('/api/auth', require('./routes/API/auth'))
+app.use('/api/profile', require('./routes/API/profile'))
+app.use('/api/posts', require('./routes/API/posts'))
+
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
