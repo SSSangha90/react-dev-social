4e91522d3d6099801823f36e8d580e753587164c
sukh <sukhveer.sangha@tatacommunications.com>
2020-06-10 18:03:47 +0100

    registering new user with gravitar and JSON response check

--- routes/API/users.js
+++ routes/API/users.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { check, validationResult } = require('express-validator/check')
+const { check, validationResult } = require("express-validator")
+const gravitar = require('gravatar')
+const bcrypt = require('bcryptjs')
+
+const User = require('../Models/User')
 
 // @route   POST api/users
 // @desc    Register user route
@@ -14,13 +18,54 @@ router.post('/', [
     check('password', 'Please enter a password with at least 6 characters')
         .isLength({min: 6})
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req)
         if(!errors.isEmpty()){
             return res.status(400)
                 .json({ errors: errors.array()})
         }
-        res.send('User route')
+
+        const { name, email, password } = req.body
+
+        try {
+        
+        // See if user exists
+        let  user = await User.findOne({ email })
+
+        if(user) {
+            res.status(400)
+            .json({errors:[{ msg: 'User already exists' }]})
+        }
+
+        // Get user's gravitar
+        const avatar = gravitar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        })
+
+        // Encrypt the password
+        const salt = await bcrypt.genSalt(10)
+
+        user.password = await bcrypt.hash(password, salt)
+
+        await user.save() // returns promise... used await
+
+        // Return JSON Web Token
+        res.send('User registered')
+
+        } catch(err){
+            console.error(err.message)
+            res.status(500).send('Server error')
+        }
+
     }
 )
 
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/profile', require('./routes/API/profile'))
 app.use('/api/posts', require('./routes/API/posts'))
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
